a49f4218f6502e7adbbc2b2d7604b1fce511d434
GreatKe <43467309+thegreatke@users.noreply.github.com>
2019-03-15 19:01:54 +0800

    315

--- MyBlog-master_3_11/src/main/java/com/zhy/MyBlogApplication.java
+++ MyBlog-master_3_11/src/main/java/com/zhy/MyBlogApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
- * @author zhouke
+ * @author zhouke  test_git 
  */
 @SpringBootApplication
 @EnableTransactionManagement
