adf9f101c5d6403524b71902718ba721554939b7
GreatKe <43467309+thegreatke@users.noreply.github.com>
2019-03-15 18:50:38 +0800

    3/15/18:50
    3/15/18:50

---
+++ MyBlog-master_3_11/src/main/java/com/zhy/mapper/ArticleLikesMapper.java
@@ -0,0 +1,25 @@
+package com.zhy.mapper;
+
+import com.zhy.model.ArticleLikesRecord;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author: zhangocean
+ * @Date: 2018/7/7 15:51
+ * Describe: 文章点赞sql
+ */
+@Mapper
+@Repository
+public interface ArticleLikesMapper {
+
+    @Insert("insert into article_likes_record(articleId,originalAuthor,likerId,likeDate) values(#{articleId},#{originalAuthor},#{likerId},#{likeDate})")
+    void insertArticleLikesRecord(ArticleLikesRecord articleLikesRecord);
+
+    @Select("select likeDate from article_likes_record where articleId=#{articleId} and originalAuthor=#{originalAuthor} and likerId=#{likerId}")
+    ArticleLikesRecord isLiked(@Param("articleId") long articleId, @Param("originalAuthor") String originalAuthor, @Param("likerId") int likerId);
+
+    @Delete("delete from article_likes_record where articleId=#{articleId}")
+    void deleteArticleLikesRecordByArticleId(long articleId);
+
+}
