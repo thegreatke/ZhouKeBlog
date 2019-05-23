6d4d137e9d156e331d74938488a5f1ccad1d891f
GreatKe <43467309+thegreatke@users.noreply.github.com>
2019-05-23 15:42:14 +0800

    2019/05/23 周四

--- MyBlog-master_3_11/src/main/java/com/zhy/constant/OSSClientConstants.java
+++ MyBlog-master_3_11/src/main/java/com/zhy/constant/OSSClientConstants.java
@@ -10,25 +10,25 @@ public class OSSClientConstants {
     /**
      * 阿里云API的外网域名
      */
-    public static final String ENDPOINT = "oss-cn-shenzhen.aliyuncs.com";
+    public static final String ENDPOINT = "oss-cn-shanghai.aliyuncs.com";
 
     /**
      * 阿里云API的密钥Access Key ID
      */
-    public static final String ACCESS_KEY_ID = "*************";
+    public static final String ACCESS_KEY_ID = "LTAIBLOdZa9DfFpV";
     /**
      *阿里云API的密钥Access Key Secret
      */
-    public static final String ACCESS_KEY_SECRET = "******************";
+    public static final String ACCESS_KEY_SECRET = "C2KzCPnsc7KsN1tUV889LFokTeV3XF";
 
     /**
      * 阿里云API的bucket名称
      */
-    public static final String BACKET_NAME = "zhy-myblog";
+    public static final String BACKET_NAME = "zk-blog-picture";
 
     /**
      * 阿里云API的文件夹名称
      */
-    public static final String FOLDER="public/";
+    public static final String FOLDER= "zk-blog-picture/";
 
 }
